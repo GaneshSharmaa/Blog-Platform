88e78067e3c59b0ff282eb3c5152d3439481b6b3
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-06-27 22:51:49 +0530

    Add user creation endpoint and update static file handling

--- main.py
+++ main.py
@@ -7,75 +7,34 @@ from starlette.exceptions import HTTPException as StarletteHTTPException
 # from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy import select
-from sqlalchemy.orm import session
+from sqlalchemy.orm import Session
+from typing import Annotated
 
 # importing local modules from other files
-from schemas import PostCreate, PostResponse
+from schemas import PostCreate, PostResponse, UserCreate, UserResponse
 import models
 from database import Base, engine, get_db
 
-from typing import Annotated
+# creating database table
+Base.metadata.create_all(bind = engine)
 
 # initializing the app
 app = FastAPI()
 
 # static folder access to the FastAPI
-app.mount("/static", StaticFiles(directory = "static"), name = "static")
+app.mount(
+    path = "/static",
+    app = StaticFiles(directory = "static"),
+    name = "static"
+)
+app.mount(
+    path = "/media",
+    app = StaticFiles(directory = "media"),
+    name = "media"
+)
 
 # creating a HTML template
 templates = Jinja2Templates(directory = "./templates")
-
-# posts data for the app (later will be shifted to database)
-posts: list[dict] = [
-    {
-        "id": 1,
-        "author": "Ganesh Sharma",
-        "title": "Getting started with AI/ML",
-        "content": "Stay tuned!",
-        "date_posted": "May 27, 2026",
-        "profile_pic": "/static/profile-picture/you.jpg"
-    },
-    {
-        "id": 2,
-        "author": "John Doe",
-        "title": "Python increase day-by-day",
-        "content": "Python is a great language.",
-        "date_posted": "May 27, 2026",
-        "profile_pic": "/static/profile-picture/you.jpg"
-    },
-    {
-        "id": 3,
-        "author": "Ben 10",
-        "title": "Increasing Global Warming — El Nino",
-        "content": "You might have noticed the heat now in summers.",
-        "date_posted": "May 24, 2026",
-        "profile_pic": "/static/profile-picture/you.jpg"
-    },
-    {
-        "id": 4,
-        "author": "Mary Jane",
-        "title": "I love SpiderMan",
-        "content": "Spiddy, you're a hero! I swear to God, for real!",
-        "date_posted": "May 25, 2026",
-        "profile_pic": "/static/profile-picture/you.jpg"
-    },
-    {
-        "id": 5,
-        "author": "Narendra Modi",
-        "title": "India 2047 — A vision",
-        "content": "The future I see for this country, I think is achievable by 2047.",
-        "date_posted": "April 30, 2026",
-        "profile_pic": "/static/profile-picture/you.jpg"
-    },
-    {
-        "id": 6,
-        "author": "Will Hunting",
-        "title": "For God sake, please stop living inside your head",
-        "content": "Stop living inside your head, and it's not your fault for not knowing earlier that time could only teach you. Whatever happened, happened, now live and cherish the present, present is what only exists, so live it.",
-        "date_posted": "May 25, 2026",
-        "profile_pic": "/static/profile-picture/you.jpg"
-    },
-]
 
 # -------- HOME PAGE -------- 
 @app.get("/", include_in_schema = False, name = "home")
@@ -118,6 +77,51 @@ def post_page(request: Request, post_id: int):
         },
         status_code = 404
     )
+
+# -------------- FOR USERS --------------
+@app.post(
+    path = "/api/users",
+    response_model = UserResponse,
+    status_code = status.HTTP_201_CREATED
+)
+def create_user(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
+    # for username
+    result = db.execute(
+        select(models.User).where(models.User.username == user.username)
+    )
+
+    existing_user = result.scalars().first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            detail = "Username already exists"
+        )
+    
+    # for email
+    result = db.execute(
+        select(models.User).where(models.User.email == user.email)
+    )
+
+    existing_email = result.scalars().first()
+
+    if existing_email:
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            detail = "Email already exists"
+        )
+    
+    # for new user, username and email both doesn't already exists
+    new_user = models.User(
+        username = user.username,
+        email = user.email
+    )
+
+    db.add(new_user)      # stages the changes
+    db.commit()           # saves and commits the changes
+    db.refresh(new_user)  # refresh the database
+
+    return new_user
 
 # -------- ALL POST API PAGE -------- 
 # returns list of posts, as response

01bd1be7a71297cb1d833d86ae8c091bbbb73851
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-06-30 01:34:22 +0530

    Add `UserUpdate` model for updating user information with optional fields

--- schemas.py
+++ schemas.py
@@ -16,6 +16,11 @@ class UserResponse(UserBase):
     image_file: str | None
     image_path: str
 
+class UserUpdate(BaseModel):
+    username: str | None = Field(default = None, min_length = 1, max_length = 25)
+    email: EmailStr | None = Field(default = None, max_length = 120)
+    image_file: str | None = Field(default = None, min_length = 1, max_length = 200)
+
 # creating a base field validation Pydantic model
 class PostBase(BaseModel):
     title: str = Field(min_length = 1, max_length = 100)

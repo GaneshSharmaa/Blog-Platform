462e83b4358e4cc0fc584c61530a0115081d41a5
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-06-30 01:53:19 +0530

    Add `PATCH` endpoint for updating user information with validation

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import Session
 from typing import Annotated
 
 # importing local modules from other files
-from schemas import PostCreate, PostResponse, UserCreate, UserResponse, PostUpdate
+from schemas import PostCreate, PostResponse, PostUpdate, UserCreate, UserResponse, UserUpdate
 import models
 from database import Base, engine, get_db
 
@@ -200,6 +200,49 @@ def get_user_posts(
 
     posts = result.scalars().all()
     return posts
+
+@app.patch(path = "/api/users/{user_id}", response_model = UserResponse)
+def update_user(user_id: int, user_update: UserUpdate, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.User).where(models.User.id == user_id))
+    user = result.scalars().first()
+    if not user:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "User not found"
+        )
+    
+    if user_update.username is not None and user_update.username != user.username:
+        result = db.execute(
+            select(models.User).where(models.User.username == user_update.username)
+        )
+        existing_user = result.scalars().first()
+        if existing_user:
+            raise HTTPException(
+                status_code = status.HTTP_400_BAD_REQUEST,
+                detail = "Username already exists"
+            )
+    
+    if user_update.email is not None and user_update != user.email:
+        result = db.execute(
+            select(models.User).where(models.User.email == user_update.email)
+        )
+        existing_email = result.scalars().first()
+        if existing_email:
+            raise HTTPException(
+                status_code = status.HTTP_400_BAD_REQUEST,
+                detail = "Email already exists"
+            )
+    
+    if user_update.username is not None:
+        user.username = user_update.username
+    if user_update.email is not None:
+        user.email = user_update.email
+    if user_update.image_file is not None:
+        user.image_file = user_update.image_file
+
+    db.commit()
+    db.refresh(user)
+    return user
 
 # -------- POST API PAGE --------
 # returns single post, as response
